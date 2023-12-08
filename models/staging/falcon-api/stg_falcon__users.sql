ee1ed44ed431053b55bb14cf21110cad1c4b90ce
Duc Jeremy Vu <duc.jeremy.vu@ucm.agency>
2023-12-08 15:46:35 +0100

    removed ucm_dagster

--- models/staging/falcon-api/stg_falcon__users.sql
+++ models/staging/falcon-api/stg_falcon__users.sql
@@ -3,16 +3,16 @@ with
     users as (
         select
             id as user_id,
-            first_name as user_first_name,
-            last_name as user_last_name,
-            locale_id as user_locale_id,
-            email as user_email,
-            gender_id as user_gender_id,
-            birth_date as user_birth_date,
-            last_login_at as user_last_login_at,
-            created_at as user_created_at,
-            deleted_at as user_deleted_at,
-            updated_at as user_updated_at
+            first_name as first_name,
+            last_name as last_name,
+            locale_id as locale_id,
+            email as email,
+            gender_id as gender_id,
+            birth_date as birth_date,
+            last_login_at as last_login_at,
+            created_at as created_at,
+            deleted_at as deleted_at,
+            updated_at as updated_at
 
         from sources.falcon_users
     )
